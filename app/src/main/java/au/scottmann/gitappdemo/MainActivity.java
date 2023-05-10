c26e0c1d4653a858f2110aa9f138e35acddae4f1
Scott Mann <s.mann@latrobe.edu.au>
2023-05-10 12:55:54 +1000

    Update Dev

--- app/src/main/java/au/scottmann/gitappdemo/MainActivity.java
+++ app/src/main/java/au/scottmann/gitappdemo/MainActivity.java
@@ -3,6 +3,7 @@ package au.scottmann.gitappdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void logToConsole() {
+        Log.d("GITDEMO", "Sample write");
     }
 }
